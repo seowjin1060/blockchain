38b9f1939db2e1ee4e0768e4c4eb7d7f17b6569d
Kang <piono623@naver.com>
2018-01-15 22:02:06 +0900

    blueprint

---
+++ blockchain.js
@@ -0,0 +1,27 @@
+class Blockchain {
+
+    constructor() {
+        this.chain = [];
+        this.current_transaction = [];
+    }
+
+    new_block() {
+        return;
+    }
+
+    new_transaction() {
+        return;
+    }
+
+    static hash() {
+        return;
+    }
+
+    get last_block() {
+        return;
+    }
+
+    set last_block() {
+        return;
+    }
+}
